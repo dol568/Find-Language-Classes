28a939e15b7bf00f163684a3b3599324b52d8bfa
Dariusz Olszewski <dariusz.olszewski@mailfence.com>
2024-03-23 14:50:07 +0100

    m

--- angular-frontend/src/app/shared/_constVars/_api_consts.ts
+++ angular-frontend/src/app/shared/_constVars/_api_consts.ts
@@ -5,4 +5,4 @@ export const _api_language_classes = 'languageClasses';
 export const _api_account = 'users';
 export const _api_login = 'login';
 export const _api_signup = 'register';
-export const _api_chat = 'http://localhost/ws';
+export const _api_chat = 'https://localhost/ws'
